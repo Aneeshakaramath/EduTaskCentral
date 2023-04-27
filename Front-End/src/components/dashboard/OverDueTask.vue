fb584f175a5b278bad46921ba1295243a835463d
riyazahmed001 <rafiriyazahmed@gmail.com>
2023-04-27 07:14:14 +0530

    expand collapse button

--- Front-End/src/components/dashboard/OverDueTask.vue
+++ Front-End/src/components/dashboard/OverDueTask.vue
@@ -1,15 +1,28 @@
 <template>
-    <div class="over-due-details-container align-center boldText">
-        <h2 class="task-status-label boldText">
-            Task Status 
-        </h2>
-        <div v-if="!isAnyTaskDone && !isAnyTaskOverDue" class="align-center boldText">
-            No Task is in done status or over due status
-        </div>
-        <div else>
-            <apexchart type="donut" width="380" :options="chartOptions" :series="series"></apexchart>
-        </div>
+  <div class="over-due-details-container align-center boldText">
+    <h2 class="task-status-label boldText">
+      Task Status
+    </h2>
+    <div v-if="!isAnyTaskDone && !isAnyTaskOverDue" class="align-center boldText">
+      No Task is in done status or over due status
     </div>
+    <div else>
+      <apexchart type="donut" width="380" :options="chartOptions" :series="series"></apexchart>
+      <p class="over-due-task-button">
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample"
+          aria-expanded="false" aria-controls="collapseExample">
+          show overdue tasks
+        </button>
+      </p>
+      <div class="collapse" id="collapseExample">
+        <ul v-for="task in taskList">
+          <RouterLink :to="{ name: 'taskById' , params : { taskId: task.id}}">
+                        {{ task.description }}
+          </RouterLink>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import { useUserStore } from '@/stores/User';
@@ -19,104 +32,117 @@ const store = useUserStore();
 
 
 const isAnyTaskDone = computed(() => {
-    let filteredArray = [];
-    if(store.userData?.taskAssignedToMe) {
-        filteredArray = store.userData.taskAssignedToMe.filter( function(task) {
-            return task!=null && task.taskStatus == 'DONE';
-        })
-    }
-    return filteredArray.length > 0;
+  let filteredArray = [];
+  if (store.userData?.taskAssignedToMe) {
+    filteredArray = store.userData.taskAssignedToMe.filter(function (task) {
+      return task != null && task.taskStatus == 'DONE';
+    })
+  }
+  return filteredArray.length > 0;
 });
 
-const isAnyTaskOverDue = computed(()=> {
-    let today = new Date()
-    if(store.userData?.taskAssignedToMe) {
-        store.userData.taskAssignedToMe.filter( function(task) {
-            var endDate = new Date(task.endDate);
-            if(today > endDate && task.taskStatus !== 'DONE') {
-                return true;
-            }
-        })
-    }
-    return false;
+const isAnyTaskOverDue = computed(() => {
+  let today = new Date()
+  if (store.userData?.taskAssignedToMe) {
+    store.userData.taskAssignedToMe.filter(function (task) {
+      var endDate = new Date(task.endDate);
+      if (today > endDate && task.taskStatus !== 'DONE') {
+        return true;
+      }
+    })
+  }
+  return false;
 })
 
+const taskList = computed(()=> {
+  let filteredArray = [];
+  if (store.userData?.taskAssignedToMe) {
+    filteredArray = store.userData.taskAssignedToMe.filter(function (task) {
+      return task != null && task.taskStatus == 'DONE';
+    })
+  }
+  return filteredArray;
+})
 const chartOptions = computed(() => {
-  return  {
-            chart: {
-              width: 250,
-              type: 'donut',
-            },
-            labels: ['OVER DUE', 'DONE'],
-            plotOptions: {
-              pie: {
-                startAngle: -90,
-                endAngle: 270
-              }
-            },
-            dataLabels: {
-              enabled: false
-            },
-            fill: {
-              type: 'gradient',
-            },
-            legend: {
-              formatter: function(val, opts) {
-                return val + " - " + opts.w.globals.series[opts.seriesIndex]
-              }
-            },
-            title: {
-              text: 'Task Consolidated View'
-            },
-            responsive: [{
-              breakpoint: 480,
-              options: {
-                chart: {
-                  width: 200
-                },
-                legend: {
-                  position: 'bottom'
-                }
-              }
-            }]
-          }
+  return {
+    chart: {
+      width: 250,
+      type: 'donut',
+    },
+    labels: ['OVER DUE', 'DONE'],
+    plotOptions: {
+      pie: {
+        startAngle: -90,
+        endAngle: 270
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    fill: {
+      type: 'gradient',
+    },
+    legend: {
+      formatter: function (val, opts) {
+        return val + " - " + opts.w.globals.series[opts.seriesIndex]
+      }
+    },
+    title: {
+      text: 'Task Consolidated View'
+    },
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+  }
 });
 
-const series = computed(()=> {
-    console.log("test series");
-    let filteredArray = [];
-    if(store.userData?.taskAssignedToMe) {
-        filteredArray = store.userData.taskAssignedToMe.filter( function(task) {
-            return task!=null && task.taskStatus == 'DONE';
-        })
-    }
-    
-    let today = new Date();
-    let count = 0;
-    if(store.userData?.taskAssignedToMe) {
-        store.userData.taskAssignedToMe.filter( function(task) {
-            var endDate = new Date(task.endDate);
-            if(today > endDate && task.taskStatus !== 'DONE') {
-                count++;
-            }
-        })
-    }
+const series = computed(() => {
+  let filteredArray = [];
+  if (store.userData?.taskAssignedToMe) {
+    filteredArray = store.userData.taskAssignedToMe.filter(function (task) {
+      return task != null && task.taskStatus == 'DONE';
+    })
+  }
 
-    return [count, filteredArray.length];
+  let today = new Date();
+  let count = 0;
+  if (store.userData?.taskAssignedToMe) {
+    store.userData.taskAssignedToMe.filter(function (task) {
+      var endDate = new Date(task.endDate);
+      if (today > endDate && task.taskStatus !== 'DONE') {
+        count++;
+      }
+    })
+  }
+
+  return [count, filteredArray.length];
 });
 </script>
   
 <style scoped>
 .over-due-details-container {
-    margin-top: 20px;
-    padding: 10px;
-    background-color: #f3eff2;
+  margin-top: 20px;
+  padding: 10px;
+  background-color: #f3eff2;
 }
 
 .task-status-label {
-    font-size: 20px;
-    color: black;
-    text-align: left;
+  font-size: 20px;
+  color: black;
+  text-align: left;
+}
+
+.over-due-task-button {
+  text-align: left;
+  margin-top: 10px;
 }
 </style>
   
